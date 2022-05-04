4ce703c52d0bec2c8c38d107c16c24c14c18b4a8
naikrahul175 <naikrahul175@gmail.com>
2022-05-04 17:27:19 +0530

    Takes input from user and print output on screen

---
+++ practice3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("Enter the value of a and b");
+    scanf("%d %d",&a,&b);
+    printf("a =%d b =%d", a,b);
+}
+
+/*
+1. how to take value from user and prints that on screen
+
+*/
