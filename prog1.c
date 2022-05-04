bb2faf9e327335d631598b10479d44e7d3d61ad6
naikrahul175 <naikrahul175@gmail.com>
2022-05-04 17:03:43 +0530

    calculate simple interest program

---
+++ prog1.c
@@ -0,0 +1,21 @@
+# include<stdio.h>
+
+int main()
+{
+    int p,n;
+    float r,si;
+
+    p=1000;
+    n =3;
+    r=8.5;
+
+//simple interest formula
+    si = p*n*r/100;
+    printf("Simple Intrest is: %f",si);
+}
+
+/*
+1. first program in c .
+2. basic idea of , how instruction are executed in c.
+3. calculate simple interest program. 
+*/
