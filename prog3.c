32151e1798ad8439ceb46db6688e10ebaa407b4f
naikrahul175 <naikrahul175@gmail.com>
2022-05-04 17:14:01 +0530

    calculate power of the number

---
+++ prog3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+int a;
+a = pow(3,2);
+printf("power: %d\n",a);
+
+}
+
+/*
+1. calculate power of 3^2
+2. to use pow function , we have used #include<math.h> header 
+
+*/
